bb0a1639294f37f7e645475e13958b823f1bf5a6
Emil Bengtsson <emil@emil0r.com>
2016-07-28 22:52:03 +0200

    deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [reverie-core "0.8.0-SNAPSHOT"]
+                 [reverie-core "0.8.0-alpha3"]
                  [com.taoensso/encore "2.68.1"]
                  [com.taoensso/nippy "2.12.1" :exclusions [[com.taoensso/encore]]]
                  [com.taoensso/carmine "2.14.0" :exclusions [[com.taoensso/nippy]]]]
